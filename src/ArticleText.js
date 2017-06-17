f2fab283fe450129d05f7daf5680cb3601ea64bd
Davillious Masayile <user@Users-MBP.attlocal.net>
2017-06-16 20:07:04 -0400

    Add comment css and js

---
+++ src/ArticleText.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './ArticleText.css'
+import AuthorInfo from './AuthorInfo'
+import ArticleText from './ArticleText'
+import ArticleLinks from './ArticleLinks'
+import Aside from './Aside'
+import OtherArticles from './OtherArticles'
+import ArticleTitle from './ArticleTitle'
+
+class ArticleText extends Component {
+  render() {
+    return (
+      <main className="expanded row">
+          <div className="large-8 medium-12 columns article">
+            <ArticleTitle />
+
+            <AuthorInfo />
+
+            <ArticleText />
+
+            <ArticleLinks />
+
+            
+            
+          </div>
+          
+          <Aside />
+
+          <OtherArticles />
+
+      </main>
+    )
+  }
+}
+
+export default ArticleText
